799e6dd1a9c630eeb54b140a4b969c4977ab917d
MD <dayclawtel@gmail.com>
2022-01-28 22:23:04 -0800

    commit tree b625f3de57a10413b1d9036f5d2bb825b7972b1b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(name)
+    batch_badge_creator(name).each do |badge|
+        puts badge
+    end
+    assign_rooms(name).each do |assign|
+        puts assign
+    end
+end
